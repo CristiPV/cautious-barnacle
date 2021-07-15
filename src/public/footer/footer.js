e5f85d581c7bfed0e1eb8d10beb391fcf8da57c3
Cristian <cristi.valentin29@gmail.com>
2021-07-15 13:02:21 +0300

    Add navbar and footer components

---
+++ src/public/footer/footer.js
@@ -0,0 +1,11 @@
+function setCopyright() {
+  const copyrightSpan = document
+    .getElementsByClassName("footer")[0]
+    .getElementsByClassName("right")[0]
+    .getElementsByClassName("copyright")[0];
+  const year = new Date().getFullYear();
+
+  copyrightSpan.innerText = "Copyright © " + year;
+}
+
+setCopyright();
